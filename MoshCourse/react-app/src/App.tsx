97a0a8b823ffd402ba3b3d3bd0c14c6c28933f64
ShadiK1999 <123907887+ShadiK1999@users.noreply.github.com>
2024-12-02 12:00:36 +1100

    add alert functionality to button

--- MoshCourse/react-app/src/App.tsx
+++ MoshCourse/react-app/src/App.tsx
@@ -1,8 +1,24 @@
+import Alert from "./components/Alert";
 import Button from "./components/Button";
+import { useState } from "react";
+
 function App() {
+  const [visible, setVisibility] = useState(false);
   return (
     <div>
-      <Button onClick={() => console.log("clicked")}>hello</Button>
+      {visible && (
+        <Alert>
+          Alert!!!
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+            onClick={() => setVisibility(false)}
+          ></button>
+        </Alert>
+      )}
+      <Button onClick={() => setVisibility(true)}>hello</Button>
     </div>
   );
 }
--- MoshCourse/react-app/src/components/Alert.tsx
+++ MoshCourse/react-app/src/components/Alert.tsx
@@ -4,7 +4,16 @@ interface Props {
   children: ReactNode;
 }
 const Alert = ({ children }: Props) => {
-  return <div className="alert alert-primary">{children}</div>;
+  return (
+    <>
+      <div
+        className="alert alert-warning alert-dismissible fade show"
+        role="alert"
+      >
+        {children}
+      </div>
+    </>
+  );
 };
 
 export default Alert;
