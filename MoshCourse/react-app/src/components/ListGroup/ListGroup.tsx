b6acc2169a070cf992ec877cd688ec2acde10fbb
ShadiK1999 <123907887+ShadiK1999@users.noreply.github.com>
2024-12-03 15:44:38 +1100

    Add styles using CSS-in-JS method

--- MoshCourse/react-app/src/components/ListGroup/ListGroup.tsx
+++ MoshCourse/react-app/src/components/ListGroup/ListGroup.tsx
@@ -1,5 +1,19 @@
 import { useState } from "react";
-import styles from "./ListGroup.module.css";
+import "./ListGroup.css";
+import styled from "styled-components";
+
+interface ListItemProps {
+  active: boolean;
+}
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const ListItem = styled.li<ListItemProps>`
+  padding: 5px 0;
+  background-color: ${(props) => (props.active ? "blue" : "none")};
+`;
 
 interface Props {
   items: string[];
@@ -7,20 +21,16 @@ interface Props {
   onSelectItem: (item: string) => void;
 }
 function ListGroup({ items, heading, onSelectItem }: Props) {
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [selectedIndex, setSelectedIndex] = useState(0);
   // ul classname changed to include multiple css styles
   return (
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>no items found</p>}
-      <ul className={[styles.listGroup, styles.container].join(" ")}>
+      <List>
         {items.map((item, index) => (
-          <li
-            className={
-              selectedIndex === index
-                ? "list-group-item active"
-                : "list-group-item"
-            }
+          <ListItem
+            active={index === selectedIndex}
             key={item}
             onClick={() => {
               setSelectedIndex(index);
@@ -28,9 +38,9 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
             }}
           >
             {item}
-          </li>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 }
