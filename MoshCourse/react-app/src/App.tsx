5a9d4f0d689cee8de483193df7ab6329c0b0ca72
ShadiK1999 <123907887+ShadiK1999@users.noreply.github.com>
2024-12-04 11:10:50 +1100

    create cart and navbar components that share state

--- MoshCourse/react-app/src/App.tsx
+++ MoshCourse/react-app/src/App.tsx
@@ -1,23 +1,13 @@
-import produce from "immer";
 import { useState } from "react";
+import NavBar from "./components/navBar";
+import Cart from "./components/Cart";
 
 function App() {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
-  ]);
-
-  const handleClick = () => {
-    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
-  };
+  const [cartItems, setCartItems] = useState(["prod1", "prod2"]);
   return (
     <div>
-      {bugs.map((bug) => (
-        <p key={bug.id}>
-          {bug.title} {bug.fixed ? "Fixed" : "New"}
-        </p>
-      ))}
-      <button onClick={handleClick}>Click Me</button>
+      <NavBar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
     </div>
   );
 }
