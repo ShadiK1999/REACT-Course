a652891a354141981f9a6cc040117ba8a70f258c
ShadiK1999 <123907887+ShadiK1999@users.noreply.github.com>
2024-12-05 08:31:34 +1100

    demonstrate useEffect dependencies

--- MoshCourse/react-app/src/App.tsx
+++ MoshCourse/react-app/src/App.tsx
@@ -1,17 +1,23 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import ProductList from "./components/ProductList";
 
 function App() {
   const ref = useRef<HTMLInputElement>(null);
-
-  //side effects kept here to be called after render
-  useEffect(() => {
-    // Side effect
-    if (ref.current) ref.current.focus();
-  });
+  const [category, setCategory] = useState("");
 
   return (
     <div>
-      <input type="text" className="form-control" />
+      <select
+        name=""
+        id=""
+        className="form-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category} />
     </div>
   );
 }
