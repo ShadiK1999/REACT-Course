d65a5c1831e66206dabd7ae25007e5dda87def5e
ShadiK1999 <123907887+ShadiK1999@users.noreply.github.com>
2024-12-05 09:04:19 +1100

    include cancelled request handling

--- MoshCourse/react-app/src/App.tsx
+++ MoshCourse/react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -6,14 +6,26 @@ interface User {
   name: string;
 }
 function App() {
+  const controller = new AbortController();
+
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
   useEffect(() => {
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
       .then((response) => setUsers(response.data))
-      .catch((error) => setError(error.message));
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+      });
+
+    return () => {
+      controller.abort();
+    };
   }, []);
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
