ddc6f858abaa121524fe71dda00db85893eb3a3e
ShadiK1999 <123907887+ShadiK1999@users.noreply.github.com>
2024-12-04 14:57:17 +1100

    ExpenseTracker: Create Expense List component

--- MoshCourse/react-app/src/App.tsx
+++ MoshCourse/react-app/src/App.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
-  return <Form />;
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbbb", amount: 20, category: "Food" },
+    { id: 3, description: "cccc", amount: 30, category: "Transportation" },
+    //... more expenses...
+  ]);
+
+  return (
+    <ExpenseList
+      expenses={expenses}
+      onDelete={(id) => setExpenses(expenses.filter((e) => e.id != id))}
+    />
+  );
 }
 
 export default App;
