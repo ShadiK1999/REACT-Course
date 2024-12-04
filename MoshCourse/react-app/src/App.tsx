1d32815748c687b4dc14de1df0ec1dc7c53b3471
ShadiK1999 <123907887+ShadiK1999@users.noreply.github.com>
2024-12-04 12:45:45 +1100

    create form and add styling with css and bootstrap

--- MoshCourse/react-app/src/App.tsx
+++ MoshCourse/react-app/src/App.tsx
@@ -1,22 +1,7 @@
-import { useState } from "react";
-import ExpandingChars from "./components/Expandingchars";
+import Form from "./components/Form";
 
 function App() {
-  return (
-    <ExpandingChars>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores ex, ea
-      blanditiis ipsa natus quae, quas eum repellat recusandae doloribus,
-      accusamus magni iste veniam tempore dignissimos nobis alias molestiae
-      suscipit unde soluta? Praesentium accusamus aperiam ipsum similique porro,
-      quae ad repellendus nostrum, magni dolorum sunt doloribus laudantium
-      magnam. Voluptas, fugit. Necessitatibus perspiciatis dolor animi id a
-      possimus aliquid atque accusantium omnis sit rerum at aut sequi obcaecati
-      explicabo ipsum exercitationem sapiente earum iste fuga ut odit, repellat
-      inventore. Qui eum, distinctio quis facilis dignissimos fugiat explicabo
-      excepturi tempore nam. Facilis ea nisi id dolore accusamus asperiores
-      laborum reiciendis aperiam dolorum.
-    </ExpandingChars>
-  );
+  return <Form />;
 }
 
 export default App;
