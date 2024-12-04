187cbbd1eb24c7d26afdc39890e9b7d1212f5e96
ShadiK1999 <123907887+ShadiK1999@users.noreply.github.com>
2024-12-04 15:10:08 +1100

    ExpenseTracker: create ExpenseFilter component

--- MoshCourse/react-app/src/App.tsx
+++ MoshCourse/react-app/src/App.tsx
@@ -1,20 +1,32 @@
 import { useState } from "react";
 import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbbb", amount: 20, category: "Food" },
     { id: 3, description: "cccc", amount: 30, category: "Transportation" },
     //... more expenses...
   ]);
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   return (
-    <ExpenseList
-      expenses={expenses}
-      onDelete={(id) => setExpenses(expenses.filter((e) => e.id != id))}
-    />
+    <>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id != id))}
+      />
+    </>
   );
 }
 
---
+++ MoshCourse/react-app/src/expense-tracker/components/ExpenseFilter.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface Props {
+  onSelectCategory: (category: string) => void;
+}
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
+  return (
+    <select
+      className="form-select"
+      onChange={(event) => onSelectCategory(event.target.value)}
+    >
+      <option value="">All categories</option>
+      <option value="Food">Food</option>
+      <option value="Utilities">Utilities</option>
+      <option value="Transport">Transport</option>
+    </select>
+  );
+};
+
+export default ExpenseFilter;
