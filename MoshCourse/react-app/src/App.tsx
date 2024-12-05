554f673990a4edf540ebe380e3a887ec6a910cad
ShadiK1999 <123907887+ShadiK1999@users.noreply.github.com>
2024-12-05 11:00:26 +1100

    create useUsers custom hook

--- MoshCourse/react-app/src/App.tsx
+++ MoshCourse/react-app/src/App.tsx
@@ -1,31 +1,7 @@
-import { CanceledError } from "./services/api-client";
-import { useEffect, useState } from "react";
 import userService, { User } from "./services/user-service";
-
+import useUsers from "./hooks/useUsers";
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((response) => {
-        setUsers(response.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message);
-        setLoading(false);
-      });
-
-    return () => {
-      cancel();
-    };
-  }, []);
-
+  const { users, error, loading, setUsers, setError } = useUsers();
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
     setUsers(users.filter((u) => u.id !== user.id));
